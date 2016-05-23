6b4f3413500141a9b77269426aa36208db9f456b
t6tn4k <t6tn4k@gmail.com>
2016-05-23 16:42:25 +0900

    Add ofxSwizzle::swap

--- src/ofxSwizzle.hpp
+++ src/ofxSwizzle.hpp
@@ -1,6 +1,7 @@
 #ifndef OFX_SWIZZLE_HPP
 #define OFX_SWIZZLE_HPP
 
+#include "./ofxSwizzle/swap.hpp"
 #include "./ofxSwizzle/vec.hpp"
 
 #endif // #ifndef OFX_SWIZZLE_HPP
---
+++ src/ofxSwizzle/swap.hpp
@@ -0,0 +1,13 @@
+#ifndef OFX_SWIZZLE_SWAP_HPP
+#define OFX_SWIZZLE_SWAP_HPP
+
+#include "./detail/swizzle_proxy.hpp"
+#include "./vec.hpp"
+
+namespace ofxSwizzle {
+
+using ofxSwizzle::detail::swap;
+
+} // namespace ofxSwizzle
+
+#endif // #ifndef OFX_SWIZZLE_SWAP_HPP
