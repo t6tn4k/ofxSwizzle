a60d5218f0be5394b9c279441af00ab5905ae982
t6tn4k <t6tn4k@gmail.com>
2016-05-25 01:39:15 +0900

    Update

--- src/ofxSwizzle.hpp
+++ src/ofxSwizzle.hpp
@@ -1,7 +1,15 @@
 #ifndef OFX_SWIZZLE_HPP
 #define OFX_SWIZZLE_HPP
 
+#include "./ofxSwizzle/apply.hpp"
+#include "./ofxSwizzle/begin.hpp"
+#include "./ofxSwizzle/comparison.hpp"
+#include "./ofxSwizzle/data.hpp"
+#include "./ofxSwizzle/end.hpp"
+#include "./ofxSwizzle/for_each.hpp"
+#include "./ofxSwizzle/make_vector.hpp"
 #include "./ofxSwizzle/swap.hpp"
+#include "./ofxSwizzle/tuple.hpp"
 #include "./ofxSwizzle/vec.hpp"
 
 #endif // #ifndef OFX_SWIZZLE_HPP
