e4e18e55e44ffe4d587ec9ddd2f33c6fbfab6535
t6tn4k <t6tn4k@gmail.com>
2016-05-26 04:04:37 +0900

    Add integer literals

--- src/ofxSwizzle/literals/scalar_swizzle.hpp
+++ src/ofxSwizzle/literals/scalar_swizzle.hpp
@@ -20,6 +20,21 @@ inline auto operator""_xxxx(long double x) -> decltype(auto)
     return ofxSwizzle::make_vector(x, x, x, x);
 }
 
+inline auto operator""_xx(unsigned long long x) -> decltype(auto)
+{
+    return ofxSwizzle::literals::operator""_xx(static_cast<long double>(x));
+}
+
+inline auto operator""_xxx(unsigned long long x) -> decltype(auto)
+{
+    return ofxSwizzle::literals::operator""_xxx(static_cast<long double>(x));
+}
+
+inline auto operator""_xxxx(unsigned long long x) -> decltype(auto)
+{
+    return ofxSwizzle::literals::operator""_xxxx(static_cast<long double>(x));
+}
+
 } } // namespace ofxSwizzle::literals
 
 #endif // #ifndef OFX_SWIZZLE_LITERALS_SCALAR_SWIZZLE_HPP
