5a1d1286d124baa7486fcbaabb7c2d289e46eb13
t6tn4k <t6tn4k@gmail.com>
2016-05-25 15:34:21 +0900

    Remove unnecessary headers

--- src/ofxSwizzle/detail/conjunction.hpp
+++ src/ofxSwizzle/detail/conjunction.hpp
@@ -1,9 +1,6 @@
 #ifndef OFX_SWIZZLE_DETAIL_CONJUNCTION_HPP
 #define OFX_SWIZZLE_DETAIL_CONJUNCTION_HPP
 
-#include <type_traits>
-#include <utility>
-
 namespace ofxSwizzle { namespace detail {
 
 template <typename... Ts>
